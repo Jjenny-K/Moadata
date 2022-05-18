4ac66bd0129f6e6006e1186f5355e4291c962c44
Jjenny-K <kkjjh0129@gmail.com>
2022-05-18 15:36:57 +0900

    (fix)Refactoring - Job 실행 API 수정

--- flaskr/utils/commons.py
+++ flaskr/utils/commons.py
@@ -1,115 +1,145 @@
-import csv
-from config import basedir
-
-from flask import jsonify, request, json
+import uuid
 import pandas as pd
 
-def task_list(read,drop):
-    """
-        task_list 추가
-    """
-    if read and drop:
-        tasklist = {
-            "read": [
-                "drop"
-            ],
-            "drop": [
-                "write"
-            ],
-            "write": []
-        }
-        return tasklist
-    elif read:
-        tasklist = {
-            "read": [
-                "drop"
-            ],
-            "drop": []
-        }
-        return tasklist
-    else:
-        tasklist = {
-            "read": []
-        }
-        return tasklist
-
-def apply(data):
-    """
-        json file에 변경된 data를 적용한 후 data를 반환
-    """
-    with open('flaskr/data/job.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent="\t")
-    return jsonify(data)
-
+from config import basedir
+from flask import jsonify, request, json, Response
 
 
 def form_data():
     """
-        새로운 job 정보를 호출
+        작성자 : 김채욱
+        클라이언트가 입력한 새로운 job 정보를 호출
     """
     job = request.args.get('name')
     column = request.args.get('column')
     return job, column
 
 
+def task_list(read, drop):
+    """
+        작성자 : 김채욱
+        task_list 추가
+    """
+    if read and drop:
+        task_list = {
+            "read": ["drop"],
+            "drop": ["write"],
+            "write": []
+        }
+    elif read:
+        task_list = {
+            "read": ["drop"],
+            "drop": []
+        }
+    else:
+        task_list = {
+            "read": []
+        }
+
+    return task_list
+
 
 def post_data(job, column):
     """
+        작성자 : 김채욱
         새로운 job 생성
     """
     new = {
-    'jobid': uuid.uuid4(),
-    'job_name': job,
-    'task_list': {"read": ["drop"], "drop":["write"], "write":[]},
-    'property': {"read": {"task_name": "read", "filename" : "path/to/a.csv", "sep" :","}, \
-        "drop" : {"task_name": "drop", "column_name": column}, \
-        "write" : {"task_name": "write", "filename" : "path/to/b.csv", "sep": ","}}
+        'jobid': uuid.uuid4(),
+        'job_name': job,
+        'task_list': {"read": ["drop"], "drop": ["write"], "write": []},
+        'property': {
+            "read": {"task_name": "read", "filename" : "path/to/a.csv", "sep": ","}, \
+            "drop": {"task_name": "drop", "column_name": column}, \
+            "write": {"task_name": "write", "filename" : "path/to/b.csv", "sep": ","}}
     }
 
+    return new
+
+
+def apply(data):
+    """
+        작성자 : 김채욱
+        job.json file에 변경된 data를 적용한 후 변경된 data를 반환
+    """
+    with open('flaskr/data/job.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent="\t")
+    return jsonify(data)
 
 
 def bring_data():
     """
-        전체 data를
+        작성자 : 김채욱
+        job.json file에 전체 data 반환
     """
     with open('flaskr/data/job.json') as f:
         data = json.load(f)
     return data
 
 
-def read(request_csv, read_path):
+def get_single_job(data, job_id):
     """
         작성자 : 강정희
-        csv > 지정된 위치 저장 및 DataFrame 리턴
+        job.json file에서 job_id와 같은 값을 가진 job data 반환
     """
-    # csv_dicts = [{k: v for k, v in row.items()}
-    #              for row in csv.DictReader(request_csv.splitlines(), skipinitialspace=True)]
-    filepath = basedir + read_path + request_csv.filename
-    request_csv.save(filepath)
+    job = None
 
-    return pd.read_csv(filepath)
+    for ele in data:
+        if ele['jobid'] == job_id:
+            job = ele
+            break
 
-
-def drop(request_csv, read_path, column_name):
-    """
-        작성자 : 강정희
-        지정된 컬럼을 제거한 DataFrame 리턴
-    """
-    df_csv = read(request_csv, read_path)
-
-    if not (column_name in df_csv.columns):
-        return jsonify({'error message': "CSV 파일 내에 지정된 column이 없습니다"})
-
-    return df_csv.drop(columns=[column_name])
+    return job
 
 
-def write(request_csv, read_path, column_name, write_path):
-    """
-        작성자 : 강정희
-        지정된 파일 경로로 csv 형식 파일 저장
-    """
-    df_csv = drop(request_csv, read_path, column_name)
-    filepath = basedir + write_path
-    df_csv.to_csv(filepath, encoding='utf-8')
+class TaskRunningProcessor:
+    dataframe = None
+    designated_path = '/flaskr/data/'
 
-    return jsonify({'message': "Success"})
+    def read(self, csv, job):
+        """
+            작성자 : 강정희
+            지정된 파일 경로로 csv 형식 파일 저장 및 DataFrame 리턴
+        """
+        task = job['property']['read']
+
+        # 입력된 csv 지정된 위치에 저장
+        read_path = task['filename']
+        filepath = basedir + self.designated_path + read_path
+        csv.save(filepath)
+
+        # csv to dataframe
+        self.dataframe = pd.read_csv(filepath, delimiter=task['sep'])
+
+        return self.dataframe
+
+    def drop(self, job):
+        """
+            작성자 : 강정희
+            지정된 컬럼을 제거한 DataFrame 리턴
+        """
+        task = job['property']['drop']
+
+        # 삭제할 column_name 확인 및 처리
+        column_name = task['column_name']
+        if not (column_name in self.dataframe.columns):
+            result = Response("{'error message': 'CSV 파일 내에 지정된 column이 없습니다.'}", status=400, mimetype='application/json')
+        else:
+            self.dataframe = self.dataframe.drop(columns=[column_name])
+            result = self.dataframe
+
+        return result
+
+    def write(self, job):
+        """
+            작성자 : 강정희
+            지정된 파일 경로로 csv 형식 파일 저장
+            + 저장된 DataFrame 리턴
+        """
+        task = job['property']['write']
+
+        # 지정된 위치에 csv 저장
+        write_path = task['filename']
+        filepath = basedir + self.designated_path + write_path
+        self.dataframe.to_csv(filepath, sep=task['sep'], index=False, encoding='utf-8')
+        return self.dataframe
--- flaskr/views/task_view.py
+++ flaskr/views/task_view.py
@@ -1,64 +1,19 @@
-from config import basedir
 import pandas as pd
 
-from flask import render_template, request, Response, json
+from flask import request, Response
 from flask.views import View
-from flaskr.utils import bring_data
-
-
-class TaskRunningProcessor:
-    dataframe = None
-
-    def read(self, csv, job):
-        """
-            작성자 : 강정희
-            csv > 지정된 위치 저장 및 DataFrame 리턴
-        """
-        task = job['property']['read']
-
-        # 입력된 csv 지정된 위치에 저장
-        read_path = task['filename']
-        filepath = basedir + read_path + csv.filename
-        csv.save(filepath)
-
-        # csv to dataframe
-        self.dataframe = pd.read_csv(filepath, delimiter=task['sep'])
-
-        return self.dataframe
-
-    def drop(self, job):
-        """
-            작성자 : 강정희
-            지정된 컬럼을 제거한 DataFrame 리턴
-        """
-        task = job['property']['drop']
-
-        # 삭제할 column_name 확인 및 처리
-        column_name = task['column_name']
-        if not (column_name in self.dataframe.columns):
-            return jsonify({'error message': "CSV 파일 내에 지정된 column이 없습니다"})
-        self.dataframe = self.dataframe.drop(columns=[column_name])
-
-        return self.dataframe
-
-    def write(self, job):
-        """
-            작성자 : 강정희
-            지정된 파일 경로로 csv 형식 파일 저장
-        """
-        task = job['property']['write']
-
-        # 지정된 위치에 csv 저장
-        write_path = task['filename']
-        filepath = basedir + write_path
-        self.dataframe.to_csv(filepath, sep=task['sep'], encoding='utf-8')
-
-        return self.dataframe
+from flaskr.utils import TaskRunningProcessor, bring_data, get_single_job
 
 
 class TaskRunView(View):
+    """
+        작성자 : 강정희
+        리뷰어 : 이형준
+        변환을 원하는 csv 파일과 task 정보를 request 받아 task 수행 후 결과 반환
+    """
     template_name = None
     methods = ['GET', 'POST']
+    result = None
 
     def __init__(self, template_name):
         self.template_name = template_name
@@ -68,55 +23,40 @@ class TaskRunView(View):
             task_processor = TaskRunningProcessor()
 
         next_tasks = job['task_list'].get(task, [])
-        print(task, next_tasks)
 
         if task.lower() == 'read':
-            task_processor.read(csv, job)
+            self.result = task_processor.read(csv, job)
         elif task.lower() == 'drop':
-            task_processor.drop(job)
+            self.result = task_processor.drop(job)
         elif task.lower() == 'write':
-            task_processor.write(job)
+            self.result = task_processor.write(job)
 
-        if task:
+        if task is not None and (type(self.result) == pd.core.frame.DataFrame):
             for task in next_tasks:
                 self.run(job, task, task_processor, csv=None)
 
+        return self.result
 
     def dispatch_request(self):
-        # return render_template(self.template_name)
-        if request.method == 'GET':
-            return render_template(self.template_name)
-
         if request.method == 'POST':
-            request_csv = request.files['fileupload']
+            request_csv = request.files['filename']
             job_id = int(request.form.get('job_id'))
 
             # request_csv 형식 예외처리
             if request_csv.content_type != 'text/csv':
-                # return jsonify({'error message': "CSV 파일을 업로드하세요."})
-                # raise TypeError(f'CSV 파일을 업로드하세요.')
-                # raise Exception(f'400 : CSV 파일을 업로드하세요.')
                 return Response("{'error message': 'CSV 파일을 업로드하세요.'}", status=400, mimetype='application/json')
 
             # job_id와 맞는 task list check
-            data = bring_data()
-            for ele in data:
-                if ele['jobid'] == job_id:
-                    job = ele
-                    break
-            # print([ele for ele in data if int(ele['jobid']) == job_id])
-            # job = [ele for ele in data if int(ele['jobid']) == job_id][0]
-            if not job:
+            try:
+                data = bring_data()
+                job = get_single_job(data, job_id)
+            except Exception as e:
                 return Response("{'error message': '지정한 작업이 없습니다.'}", status=400, mimetype='application/json')
 
             # task_list 확인 후 해당 단계 실행
+            # 모든 데이터의 흐름은 'read'로 시작한다고 가정
             first_task = 'read'
 
-            # if last_task == 'read' or (last_task == 'drop' and result_chk):
-            #     return result.to_html()
-            # else:
-            #     return result
+            result = self.run(job, first_task, task_processor=None, csv=request_csv).to_csv(index=False)
 
-            print(self.run(job, first_task, task_processor=None, csv=request_csv))
-
-            return self.run(job, first_task, task_processor=None, csv=request_csv)
+            return Response(result, status=201, mimetype='application/json')
